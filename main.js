b58ce88192cb03c9db4464f6b632e216b4e45d52
3boudi <116286877+3boudi@users.noreply.github.com>
2025-04-16 17:52:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function changeImage1() {
 function changeImage2() {
     let displayImage = document.getElementById('img2');
     if (displayImage.src.includes('1.png')) {
-        displayImage.src = '4.png';
+        displayImage.src = '3.png';
     } else {
         displayImage.src = '1.png';
     }
